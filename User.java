7bb4f725a7526ff5d9831837dba54de144caff55
KayaSuke-a <125533690+KayaSuke-a@users.noreply.github.com>
2023-12-12 17:54:46 +0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,31 @@
+package com.example.bie_daalt_02v_test;
+
+public class User {
+    String firstName, lastName, age;
+    public User(){
+
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getAge(){
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+}
+
